81dd5d76282e9d22e2923502e73edc7f70ad4247
Dustin <Dpianoa@gmail.com>
2021-02-21 17:30:33 -0800

    finished lab

---
+++ script.js
@@ -0,0 +1,14 @@
+let friends = ["Ty","Brad","Walker","Breelyn","Jip"];
+
+for(i=0;i<friends.length; i++) {
+    let friend = frineds[i]; {
+        for (t=99;t>0;t--) {
+            let negativeOne = t -1;
+            if(t>1) {
+                console.log(s+" lines of code in the file, " + t + " lines of code; "+friend+" strikes one out, clears it all out, "+negativeOne+" lines of code in the file");
+            } else  {
+                console.log("1 line of code in the file, 1 line of code; " + friend+ " strikes one out, clears it all out, no more lines of code in the file");
+            }
+        }
+    }
+};
